a749f51402434d6fa1028b525da7c188e35150a2
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-30 13:28:25 +0800

    throw exception
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,9 @@
 namespace Laravie\SerializesQuery;
 
 use Illuminate\Database\Eloquent\Builder as EloquentQueryBuilder;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Query\Builder as FluentQueryBuilder;
+use InvalidArgumentException;
 
 /**
  * Serialize query builder.
@@ -16,19 +18,30 @@ function serialize($builder): array
         return Query::serialize($builder);
     }
 
-    return Eloquent::serialize($builder);
+    if ($builder instanceof EloquentQueryBuilder || $builder instanceof Relation) {
+        return Eloquent::serialize($builder);
+    }
+
+    throw new InvalidArgumentException('Unable to serialize $builder.');
 }
 
 /**
  * Unserialize query builder.
  *
+ * @param  string|array  $serialized
  * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
  */
-function unserialize(array $payload)
+function unserialize($serialized)
 {
-    if (! (isset($payload['model']) && isset($payload['builder']))) {
+    $payload = \is_string($serialized) ? \unserialize($serialized) : $serialized;
+
+    if (\is_array($payload)) {
+        if (isset($payload['model']) && isset($payload['builder'])) {
+            return Eloquent::unserialize($payload);
+        }
+
         return Query::unserialize($payload);
     }
 
-    return Eloquent::unserialize($payload);
+    throw new InvalidArgumentException('Unable to unserialize $payload.');
 }
